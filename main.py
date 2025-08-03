5eaafd604b8ab94f61939f10d659f17a06d91ed9
mehraj-alom <mehraj.official000@gmail.com>
2025-08-03 22:01:24 +0530

    Ran full DVC pipeline and added trained model + scores

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from Classifier import logger
 from Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
@@ -6,34 +5,34 @@ from Classifier.pipeline.stage_03_training import ModelTrainingPipeline
 from Classifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
-try:
+try: 
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<")
-except Exception as e:
-    logger.exception(e)
-    raise e
+except Exception as e: 
+    logger.exception(e) 
+    raise e 
 
-
-STAGE_NAME = "Prepare Base Model stage"
+ 
+STAGE_NAME = "Prepare Base Model stage" 
 try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<") 
+    prepare_base_model = PrepareBaseModelTrainingPipeline() 
+    prepare_base_model.main() 
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<") 
+except Exception as e: 
+    logger.exception(e) 
+    raise e 
+
+
+STAGE_NAME = "Training" 
+ 
+try: 
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
-    prepare_base_model = PrepareBaseModelTrainingPipeline()
-    prepare_base_model.main()
-    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<")
-except Exception as e:
-    logger.exception(e)
-    raise e
-
-
-STAGE_NAME = "Training"
-
-try:
-    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
-    model_trainer = ModelTrainingPipeline()
-    model_trainer.main()
-    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<")
+    model_trainer = ModelTrainingPipeline() 
+    model_trainer.main() 
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<") 
 except Exception as e:
     logger.exception(e)
     raise e
